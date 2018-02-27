e91f2a254ddac5856ea47990bae2939084a25513
wibo-hyve <wibo@thehyve.nl>
2018-02-27 14:58:53 +0100

    Add example cron_job.txt and update loading historic data

--- load_transmart_data.sh
+++ load_transmart_data.sh
@@ -6,4 +6,5 @@ SCRIPT_DIR=$(dirname $0)
 cd ${SCRIPT_DIR}
 ./remove_done_files.sh
 python -m luigi --module scripts LoadTransmartStudy
+python -m luigi --module scripts TransmartApiTask
 ./remove_done_files.sh
