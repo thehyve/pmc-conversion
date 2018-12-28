adcc15d99fa894e9440f1625977d3eee519decd2
Alessia Peviani <alessia@thehyve.nl>
2018-12-28 16:02:51 +0100

    Fix unit tests

--- tests/test_csr_transformations.py
+++ tests/test_csr_transformations.py
@@ -25,9 +25,6 @@ class CsrTransformationTests(unittest.TestCase):
         dict_ = ct.read_dict_from_file('dict_file.json', path=self.test_config)
         self.assertDictEqual(dict_, ref_dict)
 
-    def test_read_data_files(self):
-        self.assertFalse(True)
-
     def test_validate_source_file(self):
         source_file = os.path.join(self.clinical_test_data, 'study.txt')
         file_prop_dict = ct.read_dict_from_file('file_headers.json', self.config)
@@ -38,7 +35,7 @@ class CsrTransformationTests(unittest.TestCase):
         source_file = os.path.join(self.dummy_test_data, 'empty_file.txt')
         file_prop_dict = ct.read_dict_from_file('file_headers.json', self.config)
         value = ct.validate_source_file(file_prop_dict, source_file, 'file_headers.json')
-        self.assertIsNone(value)
+        self.assertTrue(value)
 
 
 
@@ -46,33 +43,33 @@ class CsrTransformationTests(unittest.TestCase):
         file_prop_dict = ct.read_dict_from_file('file_headers.json', self.config)
         header_map = ct.read_dict_from_file('columns_to_csr.json', self.config)
         overlap = ct.get_overlapping_columns(file_prop_dict, header_map)
-        expected_keys = sorted(['DIAGNOSIS_ID', 'SRC_BIOSOURCE_ID', 'CID',
-                              'TUMOR_STAGE', 'CENTER_TREATMENT', 'TUMOR_TYPE',
-                              'IDAABA_PSEUDO', 'TOPOGRAPHY', 'DESCRIPTION', 'IFCREF'])
+        expected_keys = sorted(['SRC_BIOSOURCE_ID', 'ID', 'IDAA',
+                              'DESCRIPTION', 'MARK:', 'LABEL'])
 
         self.assertEqual(sorted(overlap.keys()), expected_keys)
-        self.assertEqual(sorted(['br_test2.txt', 'br_test.txt']), sorted(overlap['CENTER_TREATMENT']))
+        self.assertEqual(sorted(['biomaterial.txt', 'biosource.txt']), sorted(overlap['DESCRIPTION']))
 
+    @unittest.skip('todo')
     def test_check_column_priority(self):
         self.assertFalse(True)
 
-
+    @unittest.skip('todo')
     def test_merge_entity_data_frames(self):
         self.assertFalse(True)
 
-
+    @unittest.skip('todo')
     def combine_column_data(self):
         self.assertFalse(True)
 
-
+    @unittest.skip('todo')
     def add_biosource_identifiers(self):
         self.assertFalse(True)
 
-
+    @unittest.skip('todo')
     def test_build_csr_dataframe(self):
         self.assertFalse(True)
 
-
+    @unittest.skip('todo')
     def test_resolve_data_conflicts(self):
         self.assertFalse(True)
 
