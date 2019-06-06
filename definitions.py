269f11231530acbcbfbf051893cd8e9443e89835
Spayralbe <stefan@thehyve.nl>
2019-06-06 17:23:20 +0200

    Add test for calculate age at diagnosis

--- definitions.py
+++ definitions.py
@@ -4,3 +4,5 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).parent.resolve()
 
 TEST_DATA_DIR = ROOT_DIR.joinpath('test_data')
+TESTS_DIR = ROOT_DIR.joinpath('tests')
+TEST_EXPECTED_OUT_DIR = TESTS_DIR.joinpath('expected_out')
