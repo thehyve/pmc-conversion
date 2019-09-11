ab935e3b1277aaea30bbec8bae633ff63ec39490
Gijs Kant <gijs@thehyve.nl>
2019-09-11 12:15:37 +0200

    Fix run.sh script.

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,7 +3,7 @@ set -e
 set -x
 
 SCRIPT_DIR=$(dirname "$0")
-cd "${SCRIPT_DIR}"
+cd "${SCRIPT_DIR}/.."
 if python -m luigi --module luigi-pipeline LoadDataFromNewFilesTask; then
     echo "Pipeline finished successfully"
 else
